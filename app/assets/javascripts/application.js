db927ec034ce0b577831beb3795b3c9bddf4ac0f
Aimee Barciauskas <barciaai@gmail.com>
2012-03-18 17:16:48 -0400

    fixed assets for production

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,8 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
-//= require jquery_ujs
-//= require_tree .
+//= require_tree ./libs
+//= require code.photoswipe-3.0.4
+//= require code.photoswipe-3.0.4.min
+//= require code.photoswipe.jquery-3.0.4
+//= require code.photoswipe.jquery-3.0.4.min
